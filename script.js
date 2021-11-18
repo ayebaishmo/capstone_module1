157fa521acabb1c35005e00388c243500c38756b
ayebaishmo <ishmo255super@gmail.com>
2021-11-18 07:16:09 -0800

    Fix lint errors

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // Side mobile menu
-const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.sideMenu');
 const mobileNav = document.querySelector('.mobileNav');
 
 function closeMobileMenu() {
-    mobileNav.style.display = 'none';
-    openMenu.style.display = 'block'
+  mobileNav.style.display = 'none';
+  openMenu.style.display = 'block';
 }
 
 function openMobileMenu() {
-    mobileNav.style.display = 'block';
-    openMenu.style.display = 'none';
+  mobileNav.style.display = 'block';
+  openMenu.style.display = 'none';
 }
 
 // team Object
@@ -19,23 +18,27 @@ const team = [{
   img: 'img/avatar1.png',
   desc: ['Ayebazibwe Ishmael', 'Full stackWeb developer', '<hr>', 'Ishmael is CEO My plug, who has experience in innovating through technology and sftware, for he has enabled the plug store to function'],
 },
-  {
+{
   img: 'img/avatar2.jpg',
   desc: ['Natweta Edmon', 'Grapphics Designer', '<hr>', 'Edmond heads the graphic department, He is responisible for the brand look of our clients that are need of that service'],
-},{
+},
+{
   img: 'img/avatar5.jpg',
   desc: ['Masaba Johhn', 'Producer', '<hr>', 'John is the best producer around town, for all our  clients production are readily produced by John Masaba the man'],
-},{
+},
+{
   img: 'img/avatar3.jpg',
   desc: ['Apio Sheillah', 'Video Director', '<hr>', 'Sheillah is in charge of video and audio commercials for our clients that run on both television and radios plus social media'],
-},{
+},
+{
   img: 'img/avatar4.jpg',
   desc: ['Nazziwa Jane', 'Marketier', '<hr>', 'Jane dose not only market for My plug but also  my plug store creators who sell and and create products on my plug store'],
-},{
+},
+{
   img: 'img/avatar6.jpg',
   desc: ['Richard Nabende', 'Program cordinator','<hr>', 'Each year we handle diffrent kinds of project for our clients, Richard makes sure that projects are managed effeciently.'],
-}
-]
+},
+];
 
 // Heading of the part
 const teamBack = document.querySelector('.teamBack');
@@ -57,7 +60,6 @@ for (let i = 0; i < team.length; i += 1) {
   const teamDec = document.createElement('div');
   teamDec.className = 'teamDec';
   teamGrid.appendChild(teamDec);
-
 
 // img div
   const imgDiv = document.createElement('div');
